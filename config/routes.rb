9e6e644f23e9289d72d3b5758614c9555f680d6f
kevinsmasters <gmail@kevinmasters.com>
2023-06-18 14:48:41 -0400

    for practice, added a page to show repertoire

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'repertoire', to: 'pages#repertoire'
+  
   # Defines the root path route ("/")
   root "pages#home"
 end
